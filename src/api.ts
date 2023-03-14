d47137858437fa1b02618febd0d0eb424086b11c
Ray Epps <rayharryepps@gmail.com>
2023-03-14 14:07:19 -0600

    stringify json body

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const API = ({ prefixUrl }: { prefixUrl: string }) => ({
   post: async (path: string, options: Options) => {
     const response = await fetch(`${prefixUrl}${path}`, {
       method: 'POST',
-      body: options.body,
+      body: options.body ? JSON.stringify(options.body) : undefined,
       headers: {
         'Content-Type': 'application/json',
         'x-vessel-session-token': options.sessionToken,
